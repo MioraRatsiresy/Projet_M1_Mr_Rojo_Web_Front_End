17e75a45c15c7ab53cf1d8ba7568dac68a0cfa9f
Roussel <153292429+RousselKanteCo@users.noreply.github.com>
2024-03-01 08:28:31 +0300

    message

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -16,6 +16,7 @@ import Validation from '../../utils/validation';
 })
 export class RegisterComponent {
   loading: boolean = false;
+  showSuccessMessage: boolean = false; // Ajout de la propriété
 
   submitted: boolean = false;
   registerForm: FormGroup = new FormGroup({
@@ -60,10 +61,12 @@ export class RegisterComponent {
       next: (res) => {
         console.log('Inscription effectué avec succès');
         this.loading = false;
+        this.showSuccessMessage = true;
       },
       error: (error) => {
         this.error = error.error.message;
         this.loading = false;
+        this.showSuccessMessage = false;
       },
     });
   }
